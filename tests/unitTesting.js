ecf0ad9b9b4d167a4f87042fccdb0997baacdaaf
sayanastha39 <54332268+sayanastha39@users.noreply.github.com>
2020-01-29 12:18:10 -0500

    Update unitTesting.js
    Made changes to unit testing

--- tests/unitTesting.js
+++ tests/unitTesting.js
@@ -1,30 +1,40 @@
-const request = require('supertest');
-const expect = require('chai').expect;
-const should = require('should');
+const chai  = require('chai');
+const expect = chai.expect;
 const get = require('../handlers/index');
+ 
+describe('Get customr details', () => {
+ 
+    it('Should return a customer details that matches the provided id', (done) => {
 
-describe("Get customr details", function() {
-    it("should return a customer details that matches the provided id", async function() {
-      const stubValue = {
-        id: "1",
-        firstname: "Santa",
-        lastname: "Arson",
-        email: "arson@hotmail.com",
-        address: "234 Oak DR, Fairfax, VA, 48824"
-      };
+        const data = {
+            id: "1",
+            firstname: "Santa",
+            lastname: "Arson",
+            email: "arson@hotmail.com",
+            address: "234 Oak DR, Fairfax, VA, 48824"
+             };
 
-      const event ={
-          pathParameters: { "id":"1" }
-      }
-      
-      //const index = new Index();
-      const user = await get.handler(event);
-
-     // expect(stub.calledOnce).to.be.true;
-      expect(user.id).to.equal(stubValue.id);
-      expect(user.firstname).to.equal(stubValue.name);
-      expect(user.lastname).to.equal(stubValue.name);
-      expect(user.email).to.equal(stubValue.email);
-      expect(user.address).to.equal(stubValue.address);
-    });
-  });
+        const event = {
+            body: '',
+            headers: {},
+            httpMethod: 'GET',
+            isBase64Encoded: false,
+            path: '',
+            pathParameters: {"id":"1"},
+            queryStringParameters: {},
+            stageVariables: {},
+            requestContext: {},
+            resource: '' };
+        
+      get.handler(event,done)
+        .then( response =>{
+            expect(response.statusCode).to.equal(200);
+            expect(JSON.parse(response.body)).to.deep.equal(data);
+        })
+        .catch(err => console.log(err))
+        .finally(done)
+        ;
+        
+     });
+ 
+});
